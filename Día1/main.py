022aabda5d7ded4f941bdffc4a758ff9fb10e056
Juan Rivero <juandariver1@outlook.com>
2024-01-25 23:13:43 -0500

    Hoy

--- Día1/main.py
+++ Día1/main.py
@@ -50,26 +50,56 @@ else:
 
 # ------------------ Arrays ------------------------
     #----------- Lista [] list()---------------------
+    #Las listas se pueden modificar
 listao = list(["danna", "yolanda", "joel", "daniel"])
-print(listao[1])
+print(listao) #Devuelve la lista completa
+print(len(listao)) #Cant de objetos en una lista, en este caso 4
+print(listao[1]) #Devuelve el valor alojado en la posición 1 (Yolanda)
+listao[3] = "Modificar" #Esto SI Es valido en el código
+
 #--------------- Tupla() tuple() --------------------
+#Las tuplas no se pueden modificar, se quedan tal y como estan para siempre
 Tupla = tuple(("Manzana", "banana", "fresas"))
 print(Tupla)
-#--------------- Diccionario{} Dict() --------------------
+
+#--------------- Diccionario{"Key" : "Value",} Dict() --------------------
 Diccionario = {
     "Nombre": "Juan",
     "Edad": "16",
     "Apellido": "Rivero"
 }
+print(Diccionario) #Devuelve todo lo alojado en el dict
+print(Diccionario.get("Nombre")) # .get devuelve el valor que esta en clave
+print(Diccionario.keys()) #Devuelve todas las claves del dict
 
-print(Diccionario)
-# conjunto set, estan en desorden y no se puede repetir un valor :)))))))))))))
-#para acceder a los datos del conjunto se utiliza un blucle :)))), denada
+#--------------- Conjuntos{} -------------------
+# parecidos a las tuplas, no se accede a los elementos por su índice y tampoco almacena datos duplicados
+conjunto =  {"Juan","Juandariver9", 16, "Tutankamon", "Juandariver9"}
+print(conjunto) # {16, 'Juandariver9', 'Tutankamon', 'Juan'} El elemento juandariver9 al estar repetido solo se pone una vez
+#print(conjunto[3]) --> No puede acceder al elemento de su índice
 
-   
+#--------------------- Funciones def <nombre>(): ----------------
+#Sin parametros y sin retorno
+def SinSin():
+    print("Hola Mundo!")
+SinSin()
+#Sin parametros y con retorno
+def SinCon():
+    c = 3 + 5
+    return c
+print(SinCon())
+#Con Parametors y sin retorno
+def ConSin(a, b):
+    c = a + b
+    print("La suma es: ", c)
+ConSin(10,20)
+#Con Parametros y Con retorno
+def ConCon(x,y):
+    n = x / y
+    return round(n,2) #round es pa redondear dahh, y el 2 es para cuantos decimales se dejan
+x = float(input("Digite un número: "))
+y = float(input("Digite otro número: "))
 
-
+print (ConCon(x,y))
     
-
-
 #Desarrollado por Juan David Rivero Romero 1096701639
