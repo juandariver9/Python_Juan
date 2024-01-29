ad323799ec9e6bfd7bd8765ebc0d1c83cd8cd262
Juan Rivero <juandariver1@outlook.com>
2024-01-29 01:57:13 -0500

    Completado

--- Dia #2/Fibonacci_RiveroRomeroJuanDavid.py
+++ Dia #2/Fibonacci_RiveroRomeroJuanDavid.py
@@ -1,27 +1,23 @@
-def fibonacci_sequence(n):
-    fib_sequence = [0, 1]
-    for i in range(2, n + 1,1):
-        fib_sequence.append(fib_sequence[i - 1] + fib_sequence[i - 2])
-    return fib_sequence
+def Secuencia(n):
+    Fib = [0, 1]
+    for i in range(2, n):
+        Fib.append(Fib[i - 1] + Fib[i - 2])
+    return Fib
 
-def main():
-    print("¡Bienvenido al generador de la Secuencia de Fibonacci!")
 
-    while True:
-        try:
-            n = int(input("\nIngrese el valor de 'n' para generar la secuencia (o ingrese 0 para salir): "))
-            if n < 0:
-                print("Por favor, ingrese un valor no negativo.")
-                continue
-            elif n == 0:
-                print("¡Hasta luego! Gracias por usar el generador de Fibonacci.")
-                break
+print("Bienvenido al generador de la Secuencia de Fibonacci")
 
-            fib_sequence = fibonacci_sequence(n)
-            print(f"\nLa Secuencia de Fibonacci hasta el término {n} es: {fib_sequence}")
-
-        except ValueError:
-            print("Por favor, ingrese un valor entero válido.")
+while True:
+    try:
+        n = int(input("\nIngrese el valor de 'n' para generar la secuencia (o ingrese 0 para salir): "))
+        if n < 0:
+            print("Por favor, ingrese un valor no negativo.")
+            continue
+        elif n == 0:
+            print("Finalizaste el generador de Fibonacci.")
+            break
+        Fib = Secuencia(n)
+        print("\nLa Secuencia de Fibonacci hasta el término ",n," es: ",Fib)
+    except:
+        print("Ingrese un valor entero valido")
 #-----------------------------------------------------------------
-
-main()
---
+++ Dia #2/NumAleatorio_RiveroRomeroJuanDavid.py
@@ -0,0 +1,29 @@
+import random
+
+print("Bienvenido al juego de adivinanza\nIntenta adivinar el número secreto entre 1 y 100")
+
+numero_secreto = random.randint(1, 100)
+intentos_totales = 10
+
+for intento_actual in range(1, intentos_totales + 1):
+    try:
+        Hipotesis = int(input(f"\nIntento {intento_actual}/{intentos_totales}. Ingresa tu Hipotesis: ")) #Solo me sirve con f strings
+        #Hipotesis = int(input("Intento ",intento_actual," / ", intentos_totales, ". Ingresa tu hipotesis:")) <- no me dejo
+        #Hipotesis = int(input("Intento " + intento_actual + " / "+ intentos_totales + ". Ingresa tu hipotesis:")) <- tampoco
+
+        if Hipotesis < 1 or Hipotesis > 100:
+            print("Ingrese un número entre 1 y 100")
+            continue
+
+        if Hipotesis == numero_secreto:
+            print(f"\n¡Felicidades! adivinaste el número secreto ({numero_secreto}) en {intento_actual} intentos :DD")
+            break
+        elif Hipotesis < numero_secreto:
+            print("El número secreto es mayor. Intenta de nuevo")
+        else:
+            print("El número secreto es menor. Intenta de nuevo")
+
+    except:
+        print("Ingrese un número valido")     
+else:
+    print(f"\nJa! Perdiste :( El número secreto era {numero_secreto}")
