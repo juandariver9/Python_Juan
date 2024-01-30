ead3d542dd48fd2ada520cc5689b98c89f9a4748
Juan Rivero <juandariver1@outlook.com>
2024-01-30 14:49:57 -0500

    maldita vida

---
+++ Dia #4/Ejercicio1.py
@@ -0,0 +1,7 @@
+T = int(input("Digite la cantidad de casos"))
+
+for i in range (0,T,1): 
+    n = int(input("Digite la cantidad de elementos en la lista y k: "))
+    list.n()
+    print(n)
+        
