cef445cbbc7ff1eaf3e7b1980b2c53e878a43161
Juan Rivero <juandariver1@outlook.com>
2024-01-30 07:14:15 -0500

    Completo

--- Dia #3/SegundoIntento.py
+++ Dia #3/SegundoIntento.py
@@ -8,10 +8,12 @@ print(num_descompose(numero))
 #Desarrollado por Juan David Rivero Romero 1096701639
 # :DDDD
 
-#x = int(input("digite un número"))
+#def numeros(x):
+#    a = [10] * (x // 10)
+#    b = [5] * ((x % 10) // 5)
+#    c = [1] * ((x % 10) % 5)
+#    return a + b + c
+    
+#x = int(input("digite un número "))
 
-#a = [10] * (x // 10)
-#b = [5] * ((x % 10) // 5)
-#c = [1] * ((x % 10) % 5)
-
-#print( a + b + c)
+#print(numeros(x))
