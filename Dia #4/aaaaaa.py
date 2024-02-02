2f9e34f13fe52089f1f5c79604b3a314f03fb2e8
Juan Rivero <juandariver1@outlook.com>
2024-02-02 07:05:53 -0500

    arreglos

--- Dia #4/aaaaaa.py
+++
@@ -1,5 +0,0 @@
-from math import gcd  
-x=300  
-y=33880  
-print("El máximo común divisor entre {} y {} es: ".format(x,y))  
-print(gcd(x,y))  
--- Dia #4/asd.py
+++
@@ -1,4 +0,0 @@
-    # Imprimir cada par encontrado en el formato especificado
-    #for par in pares:
-    #    print("(", par[0], ",", par[1], ")", sep="", end=" ")
-    #print()
