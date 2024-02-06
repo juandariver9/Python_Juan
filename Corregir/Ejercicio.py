7f3a85f1ad69d0770b2faca55e26e2b38d57cc56
Juan Rivero <juandariver1@outlook.com>
2024-02-06 08:59:39 -0500

    Corregir

---
+++ Corregir/Ejercicio.py
@@ -0,0 +1,45 @@
+def calcular_definitiva(lista):
+    lista[0]=lista[0]*0.3
+    lista[1]=lista[1]*0.3
+    lista[2]=lista[2]*0.4
+    sumatoria=0
+    for i in range(len(lista)):
+        sumatoria=sumatoria+lista[i]
+
+    return sumatoria
+
+
+for i in range(5):
+    print("Estudiante#",i+1)
+    lista=[]
+    notaMayor=0
+    definitiva=0
+    for i in range(3):
+        if(i==2):
+            numerito=(float(input("Ingresa tu nota:")))
+            print(numerito)
+            definitiva=definitiva+numerito
+            if(numerito>notaMayor):
+                notaMayor=numerito
+            lista.append(numerito)
+        else:
+            numerito=(float(input("Ingresa tu nota:")))
+            print(numerito)
+            definitiva=definitiva+numerito
+            if(numerito>notaMayor):
+                notaMayor=numerito
+            lista.append(numerito)    
+    print(lista)
+    definitiva=calcular_definitiva(lista)
+    print("Tu definitiva es:",definitiva)
+    if(definitiva<2):
+        print("Perdiste tu vida.")
+    elif (definitiva<3):
+        print("Pues... todo bien...recupera")
+    elif(definitiva>4.5):
+        print("Excelente estudiante! Sos ejemplar")
+    print(lista)
+    if(lista.index(notaMayor)==2):
+        print("Tu nota mayor es:",((notaMayor*100)/40))
+    else:
+        print("Tu nota mayor es:",((notaMayor*100)/30))
