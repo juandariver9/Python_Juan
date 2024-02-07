1bf10cc96c9081d2d2b81688201f94b9a380a330
Juan Rivero <juandariver1@outlook.com>
2024-02-07 17:01:28 -0500

    Json

--- jason/Ejercicios.py
+++ jason/Ejercicios.py
@@ -1,9 +1,30 @@
 import json
-entrada = 'aea.json'
-with open(entrada,'r') as archivo:
-    datos = json.load(archivo)
 
-for llave , valor in datos.items():
-    for llave, valor in datos['ventas'].items():
-        print(llave,valor)
-print(type(datos))
+with open("aea.json",'r') as archivo_json:
+    ejercicio = json.load(archivo_json)
+#-----------------------Ejercicio 1-----------------------
+def obtener_fecha(pedido):
+    return pedido["fecha"]
+
+pedidos = ejercicio["ventas"]["pedidos"]
+
+pedidos_ordenados = sorted(pedidos, key=obtener_fecha, reverse=True)
+
+#print(pedidos_ordenados)
+
+# Escribir los pedidos ordenados en un nuevo archivo JSON
+with open('Ejercicio1.json', 'w') as outfile:
+    json.dump(pedidos_ordenados, outfile, indent=2)
+#---------------------------------------------------------
+def obtener_fecha(pedido):
+    return pedido["total"]
+
+pedidos = ejercicio["ventas"]["pedidos"]
+
+pedidos_ordenados = sorted(pedidos, key=obtener_fecha, reverse=True)[:2]
+
+#print(pedidos_ordenados)
+
+# Escribir los primeros dos pedidos ordenados en un nuevo archivo JSON
+with open('Ejercicio2.json', 'w') as outfile:
+    json.dump(pedidos_ordenados, outfile, indent=2)
